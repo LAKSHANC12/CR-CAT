2ed2b2414253f2d3838bc8e98aa3d1701b388c47
lakshan chamodya <lakshanc965@gmail.com>
2022-10-01 22:52:17 -0700

    inisiyal commint

---
+++ src/HomeCC.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import {Text, TouchableOpacity, View} from 'react-native';
+
+import styles from './HomeCC.styles';
+
+class HomeCC extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      count: 0,
+    };
+  }
+
+  onButtonPress() {
+    this.setState({
+      count: this.state.count + 2,
+    });
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.title}>{this.state.count}</Text>
+          
+        <TouchableOpacity
+          style={styles.btn}
+          onPress={() => this.onButtonPress()}>
+          <Text style={styles.btnTitle}>+ Button</Text>
+        </TouchableOpacity>
+      </View>
+    );
+  }
+}
+
+export default HomeCC;
---
+++ src/HomeCC.styles.js
@@ -0,0 +1,28 @@
+import {StyleSheet} from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+
+  
+  title: {
+    fontSize: 30,
+    color: '#0000FF',
+    padding: 20,
+  },
+
+  btn: {
+    backgroundColor: '#00FF00',
+    padding: 15,
+  },
+
+  btnTitle: {
+    fontSize: 26,
+    color: '#333333',
+  },
+});
+
+export default styles;
